3dd4d88e20535fa6b05dc0a39029b9d7add544d4
Chaitanya Peramsetty <chaitanya.peramsetty@gmail.com>
2017-04-11 22:59:03 -0500

    part1 work done

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#	 http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -29,7 +29,10 @@ def getMyPosts():
 
 	return [ "My first post", "My second post" ]
 
-class Posts(db.Model):
+def blog_key(name = 'default'):
+	return db.Key.from_path('blogs', name)
+	
+class BlogPost(db.Model):
 	title = db.StringProperty(required = True)
 	content = db.TextProperty(required = True)
 	created_on = db.DateTimeProperty(auto_now_add = True)
@@ -49,14 +52,15 @@ class Handler(webapp2.RequestHandler):
 		self.response.write("Oops! Something went wrong.")
 		
 class MainHandler(Handler):
-	""" Handles requests coming in to '/' (the root of our site)
+	""" Handles requests coming in to '/' or '/blog' (the root of our site)
 		e.g. www.myblogpost.com/
 	"""
 
 	def get(self):
 		t = jinja_env.get_template("frontpage.html")
 		error = cgi.escape(self.request.get("error"), quote=True)
-		content = t.render(posts=getMyPosts(), error=error)
+		latest_posts = db.GqlQuery("SELECT * FROM BlogPost ORDER BY created_on DESC LIMIT 5")
+		content = t.render(posts=latest_posts, error=error)
 		self.response.write(content)
 
 
@@ -70,30 +74,47 @@ class NewPost(Handler):
 	def post(self):
 		new_blog_title = self.request.get("title")
 		new_blog_content = self.request.get("content")
-
-		# if the user typed nothing at all, redirect and yell at them
-		if (not new_blog_title) or (new_movie_title.strip() == ""):
-			error = "Please specify the title and content you want to post in the blog."
-			self.redirect("/?error=" + cgi.escape(error))
-
-		# if the user wants to add a terrible movie, redirect and yell at them
-		if new_movie_title in terrible_movies:
-			error = "Trust me, you don't want to add '{0}' to your Watchlist.".format(new_movie_title)
-			self.redirect("/?error=" + cgi.escape(error, quote=True))
-
+		
 		# 'escape' the user's input so that if they typed HTML, it doesn't mess up our site
-		new_movie_title_escaped = cgi.escape(new_movie_title, quote=True)
-
-		# construct a movie object for the new movie
-		movie = Movie(title = new_movie_title_escaped)
-		movie.put()
-
+		new_blog_title_escaped = cgi.escape(new_blog_title, quote=True)
+		new_blog_content_escaped = cgi.escape(new_blog_content, quote=True)
+		
+		'''		
+		# construct a blogpost object for the new BlogPost
+		blogpost = BlogPost(title = new_blog_title_escaped, content = new_blog_content_escaped)
+		blogpost.put()
+		
+		self.redirect("/blog")
+		'''
+		
+		if new_blog_title_escaped and new_blog_content_escaped:
+			blogpost = BlogPost(title = new_blog_title_escaped, content = new_blog_content_escaped)
+			blogpost.put()
+			self.redirect("/blog")
+			#self.redirect('/blog/%s' % str(blogpost.key().id()))
+		else:
+			error = "Title and content must be given before posting!"
+			#self.render("newpost.html", title = new_blog_title_escaped, content = new_blog_content_escaped, error=error)
+			t = jinja_env.get_template("newpost.html")
+			content = t.render(title = new_blog_title_escaped, content = new_blog_content_escaped, error=error)
+			self.response.write(content)
+			
+		# self.response.write("Your post has been submitted...")
+		'''
 		# render the confirmation message
 		t = jinja_env.get_template("add-confirmation.html")
 		content = t.render(movie = movie)
 		self.response.write(content)
+		'''
+
+class ViewPostHandler(Handler):
+	def get(self, id):
+		#pass #replace this with some code to handle the request
+		self.response.write(id)
 		
 app = webapp2.WSGIApplication([
-    ('/', MainHandler),
-	('/newpost', NewPost)
-], debug=True)
+	('/', MainHandler),
+	('/blog', MainHandler),	
+	('/newpost', NewPost),
+	('/blog/([0-9]+)', ViewPostHandler)
+	], debug=True)
